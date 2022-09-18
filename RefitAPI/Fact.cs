6dc690dcfc50e56a1ad8148ec06989f53fc04be9
BakhshiyevM <mbakhshiyev@pasha-insurance.az>
2022-09-18 04:37:19 +0400

    Add project files.

---
+++ RefitAPI/Fact.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace RefitAPI
+{
+	public class CatFact
+	{
+		[JsonPropertyName("fact")]
+		public string Fact { get; set; }
+		[JsonPropertyName("length")]
+		public int Length { get; set; }
+	}
+}
---
+++ RefitAPI/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Refit;
+using RefitAPI;
+
+Console.WriteLine("Hello, World!");
+
+
+var usersClient = RestService.For<CatAPI>("https://catfact.ninja");
+var users = await usersClient.GetFacts();
+;
